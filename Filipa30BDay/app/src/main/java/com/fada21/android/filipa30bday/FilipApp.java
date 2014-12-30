3e2bc9b85886f83ffb80c84d4539a0053b109d0e
fada21 <lukasz.czaplicki@gmail.com>
2014-12-30 22:46:10 +0100

    new covers added, ditties handling, palette animations

--- Filipa30BDay/app/src/main/java/com/fada21/android/filipa30bday/FilipApp.java
+++ Filipa30BDay/app/src/main/java/com/fada21/android/filipa30bday/FilipApp.java
@@ -2,16 +2,9 @@ package com.fada21.android.filipa30bday;
 
 import android.app.Application;
 
-import com.squareup.okhttp.Cache;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
-import java.io.File;
-import java.io.IOException;
-
 import lombok.Getter;
-
 
 public class FilipApp extends Application {
 
@@ -30,25 +23,8 @@ public class FilipApp extends Application {
     }
 
     private void initPicasso() {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Cache cache = null;
-        try {
-            cache = new Cache(getCacheDir(), 50 * 1024 * 1024);
-        } catch (IOException e) {
-            // TODO log
-        }
-        okHttpClient.setCache(cache);
-        OkHttpDownloader okHttpDownloader = new OkHttpDownloader(okHttpClient);
-        picasso = (new Picasso.Builder(getApplicationContext())).downloader(okHttpDownloader).build();
+        picasso = (new Picasso.Builder(getApplicationContext())).loggingEnabled(BuildConfig.DEBUG).build();
 
     }
 
-    private File createCacheDir() {
-        File cache = new File(getCacheDir(), "filip_covers_cache");
-        if (!cache.exists()) {
-            //noinspection ResultOfMethodCallIgnored
-            cache.mkdirs();
-        }
-        return cache;
-    }
 }
--- Filipa30BDay/app/src/main/java/com/fada21/android/filipa30bday/fragments/FilipCoverFragment.java
+++ Filipa30BDay/app/src/main/java/com/fada21/android/filipa30bday/fragments/FilipCoverFragment.java
@@ -1,8 +1,13 @@
 package com.fada21.android.filipa30bday.fragments;
 
+import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +15,8 @@ import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.graphics.Palette;
+import android.text.Html;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,11 +37,13 @@ import lombok.NoArgsConstructor;
 public class FilipCoverFragment extends Fragment {
 
     private static final String ARG_FILIP_COVER = "FILIP_COVER";
+    private static final int ALPHA_FILTER = 0xAAFFFFFF;
 
     private FilipCover filipCover;
 
     private TextView tvDitty;
     private ImageView img;
+    private boolean detached;
 
     public static FilipCoverFragment newInstance(FilipCover filipCover) {
         FilipCoverFragment fragment = new FilipCoverFragment();
@@ -59,8 +68,14 @@ public class FilipCoverFragment extends Fragment {
 
         FilipCover filipCover = getFilipCover(savedInstanceState);
 
+        String ditty = filipCover.getDitty();
+        if (!TextUtils.isEmpty(ditty)) {
+            tvDitty.setVisibility(View.VISIBLE);
+            tvDitty.setText(Html.fromHtml(ditty));
+        } else {
+            tvDitty.setVisibility(View.GONE);
+        }
 
-        tvDitty.setText(filipCover.getDitty());
         tvDitty.setOnClickListener(v -> Toast.makeText(getActivity(), "click", Toast.LENGTH_SHORT).show());
 
         return rootView;
@@ -70,27 +85,45 @@ public class FilipCoverFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        FilipApp.getInstance().getPicasso().load(filipCover.getUrl()).noPlaceholder().error(R.drawable.ic_no_img).fit().centerInside().into(img, new Callback.EmptyCallback() {
+        FilipApp.getInstance().getPicasso().load(filipCover.getUrl()).noPlaceholder().error(R.drawable.ic_no_img).fit().centerInside().into(img, getFilipCoverCallback());
+    }
 
+    private Callback getFilipCoverCallback() {
+        Callback.EmptyCallback emptyCallback = new Callback.EmptyCallback() {
+
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void onSuccess() {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                if (!detached && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
                     Bitmap bitmap = ((BitmapDrawable) img.getDrawable()).getBitmap();
                     Palette.generateAsync(bitmap, palette -> {
-                        Integer colorFrom = getResources().getColor(R.color.colorPrimaryDark);
-                        Integer colorTo = palette.getDarkMutedColor(R.color.colorPrimary);
-                        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                        Integer colorFromBg = getResources().getColor(R.color.colorPrimaryDarkAlpha);
+                        Integer colorToBg = palette.getDarkMutedColor(R.color.colorPrimaryDark);
+                        colorToBg &= ALPHA_FILTER;
+                        Integer colorFromText = getResources().getColor(android.R.color.white);
+                        Integer colorToText = palette.getLightVibrantColor(android.R.color.white);
+                        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFromBg, colorToBg);
                         colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                             @Override
                             public void onAnimationUpdate(ValueAnimator animator) {
-                                // TODO update ditty text and apla
+                                tvDitty.getBackground().setColorFilter(new PorterDuffColorFilter((Integer) animator.getAnimatedValue(), PorterDuff.Mode.DST_OVER));
                             }
                         });
-                        colorAnimation.start();
+                        ValueAnimator colorTextAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFromText, colorToText);
+                        colorTextAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animator) {
+                                tvDitty.setTextColor((Integer) animator.getAnimatedValue());
+                            }
+                        });
+                        AnimatorSet set = new AnimatorSet();
+                        set.playTogether(colorTextAnimation, colorAnimation);
+                        set.start();
                     });
                 }
             }
-        });
+        };
+        return emptyCallback;
     }
 
     private FilipCover getFilipCover(Bundle savedInstanceState) {
@@ -112,4 +145,15 @@ public class FilipCoverFragment extends Fragment {
         return filipCover;
     }
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        detached = false;
+    }
+
+    @Override
+    public void onDetach() {
+        detached = true;
+        super.onDetach();
+    }
 }
